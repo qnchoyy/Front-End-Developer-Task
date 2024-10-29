3c65421c941f7ffd6c2dfecd076695f8304a8cf0
qnchoyy <qni191488@abv.bg>
2024-10-29 08:33:51 +0200

    add components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import BlogSection from "./components/BlogSection/BlogSection";
+import CaseStudySection from "./components/CaseStudySection/CaseStudySection";
+import Expertise from "./components/Expertise/Expertise";
+import FaqSection from "./components/FaqSection/FaqSection";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Proccess from "./components/Proccess/Proccess";
+import OurResults from "./components/Results/OurResults";
+import ServicesSection from "./components/ServicesSections/ServicesSection";
+import SubscriptionForm from "./components/SubscriptionForm/SubscriptionForm";
+import Testimonials from "./components/Testimonials/Testimonial";
+
 function App() {
   return (
     <>
-      <h1>Test</h1>
+      <Header />
+      <Hero />
+      <OurResults />
+      <ServicesSection />
+      <CaseStudySection />
+      <Expertise />
+      <Testimonials />
+      <Proccess />
+      <FaqSection />
+      <BlogSection />
+      <SubscriptionForm />
+      <Footer />
     </>
   );
 }
